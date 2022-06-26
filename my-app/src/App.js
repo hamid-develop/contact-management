2857164929ac6e11382d71227b0ed76c510aadd7
hamid-develop <h.alizadeh87@hotmail.com>
2022-06-26 15:26:39 +0430

    project created

---
+++ my-app/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="w-full h-80 bg-red-400">
+   <h1 className=" text-center">Hamidreza</h1>
+    </div>
+  );
+}
+
+export default App;

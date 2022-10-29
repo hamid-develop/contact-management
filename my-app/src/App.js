44e5986ee1215fbc4c60add4c46f388e62b63417
hamid-develop <h.alizadeh87@hotmail.com>
2022-10-29 18:49:56 +0330

    App component updated..

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,7 +3,7 @@ import { Link, Outlet } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <nav className="flex items-center justify-center border-b h-20 p-5">
+      <nav className="flex items-center justify-center border-b h-20 p-4">
         <Link to="/about">About</Link>{" "}<Link to="/persons">Persons</Link>
       </nav>
         <Outlet />
